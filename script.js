c800ca5c376d87a83e830cf07337155ad1a01b67
iRobin93 <61976533+iRobin93@users.noreply.github.com>
2024-08-12 19:33:40 +0200

    teleportering

--- script.js
+++ script.js
@@ -1,7 +1,48 @@
-function objectPressed(){
 
+let objectSelected;
+
+
+function objectPressed(element) {
+    if (element == objectSelected) {
+        objectSelected.classList.toggle('dashedLine');
+        objectSelected = undefined;
+        toggleGridItemsPointer();
+    }
+
+    else {
+        if (objectSelected != undefined)
+            objectSelected.classList.toggle('dashedLine');
+        else
+            toggleGridItemsPointer();
+
+        objectSelected = element;
+        element.classList.toggle('dashedLine');
+    }
 }
 
-function gridPressed(){
-    
+
+function gridItemPressed(thisGridItem) {
+
+    if (thisGridItem.children[0].src != "") {
+        let myItemArray = document.getElementsByClassName('item');
+        for (i = 0; i < myItemArray.length; i++)
+            if (myItemArray[i].children[0].src == thisGridItem.children[0].src) {
+                myItemArray[i].children[0].style.visibility = "visible";
+                thisGridItem.children[0].removeAttribute('src');
+            }
+    }
+    else if (objectSelected != undefined) {
+        thisGridItem.children[0].src = objectSelected.src;
+        objectSelected.style.visibility = "hidden";
+        objectSelected.classList.toggle('dashedLine');
+        objectSelected = undefined;
+        toggleGridItemsPointer();
+    }
+}
+
+
+function toggleGridItemsPointer() {
+    let myGridArray = document.getElementsByClassName('grid-item');
+    for (i = 0; i < myGridArray.length; i++)
+        myGridArray[i].classList.toggle("pointer");
 }
